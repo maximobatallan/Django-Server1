d3e215445776f7ffdde0320f658be192f94b0c85
maximobatallan <71332822+maximobatallan@users.noreply.github.com>
2023-10-24 01:13:46 -0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,6 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+
+CSRF_TRUSTED_ORIGINS = ['https://*.up.railway.app']
 # FORM SUBMISSION
 # Comment out the following line and place your railway URL, and your production URL in the array.
 # CSRF_TRUSTED_ORIGINS = ["*"]
