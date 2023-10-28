d647867b9d1665eb898ac69b2b99d3735968f67b
maximobatallan <71332822+maximobatallan@users.noreply.github.com>
2023-10-28 02:46:31 -0300

    Add files via upload

--- tasks/views.py
+++ tasks/views.py
@@ -51,15 +51,21 @@ def signout(request):
 
 def signin(request):
     if request.method == 'GET':
+        
         return render(request, 'signin.html', {"form": AuthenticationForm})
     else:
-        user = authenticate(
-            request, username=request.POST['username'], password=request.POST['password'])
-        if user is None:
-            return render (request, 'signin.html',{'form': AuthenticationForm, "error":'Usuario o pass incorrecto'})
+        if 'submit_get' in request.POST:
+            return render(request, 'signin.html', {"form": AuthenticationForm})
         else:
-            login(request, user)
-            return redirect('tasks')
+            user = authenticate(
+                request, username=request.POST['username'], password=request.POST['password'])
+            if user is None:
+                return render (request, 'signin.html',{'form': AuthenticationForm, "error":'Usuario o pass incorrecto'})
+            else:
+                
+
+                login(request, user)
+                return redirect('tasks')
 
 @login_required
 def create_task(request):
