18a1f6cebbc093817de80fa5c37ef9ee4b3a3f90
maximobatallan <71332822+maximobatallan@users.noreply.github.com>
2023-10-27 23:07:22 -0300

    Add files via upload

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,8 @@
-"""mysite URL Configuration
+"""
+URL configuration for Mecha project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,10 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from tasks import views
-
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
     path('tasks/', views.tasks, name='tasks'),
@@ -40,3 +41,6 @@ urlpatterns = [
     path('productdetails/<int:producto_id>/', views.detalleproducto, name='PD'),
     path('cart/', views.cart, name='cart'),
     ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
