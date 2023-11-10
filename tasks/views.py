8e655cea526c9694ecab743abb7a71c26d171781
maximobatallan <71332822+maximobatallan@users.noreply.github.com>
2023-11-10 00:26:02 -0300

    Add files via upload

--- tasks/views.py
+++ tasks/views.py
@@ -3,27 +3,46 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
-from .forms import TaskForm
-from .models import Task, Producto
+from .forms import TaskForm, DatosForm, ProductForm
+from .models import Task, Producto, DatosPersonales
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 from .carrito import Carrito
 import mercadopago
 from django.http import JsonResponse
-
+from django.core.mail import send_mail
 
 
 def home(request):
-    productos = Producto.objects.all()
+    productos = Producto.objects.filter(important=True)
+
+    # Creamos un diccionario para agrupar los productos por categoría
+    categorias_productos = {}
+    
+    for producto in productos:
+        id = producto.id
+        categoria = producto.cat
+        print(producto.id)
+
+        if categoria in categorias_productos:
+            categorias_productos[categoria].append(id)
+        else:
+            categorias_productos[categoria] = [id]
+    
+    # Imprimimos el diccionario
+    #for categoria, producto in categorias_productos.items():
+    print(categorias_productos)   
    
-    return render(request, 'gallery.html',{'productos': productos})
+    return render(request, "home.html", {'categorias_productos': categorias_productos, 'productos': productos} )
+
+
+
 
 def signup(request):
     if request.method == 'GET':
         
         return render(request, 'signup.html',{'form': UserCreationForm})
-        
     else:
         if request.POST['password1'] == request.POST['password2']:
             try:
@@ -69,6 +88,7 @@ def signin(request):
                 login(request, user)
                 return redirect('gallery')
 
+
 @login_required
 def create_task(request):
     
@@ -83,6 +103,7 @@ def create_task(request):
             return redirect('tasks')
         except ValueError:
             return render(request, 'create_task.html', {'form': TaskForm, 'error':'Por favor ingresos los datos validos'})
+     
         
 @login_required
 def task_detail(request, task_id):
@@ -98,6 +119,39 @@ def task_detail(request, task_id):
             return redirect('tasks')
         except ValueError:
             return render(request, 'task_detail.html', {'task': task, 'form':form, 'error':'Error de actualizacion'})
+        
+        
+@login_required
+def datos(request):
+    if request.method == 'GET':
+        return render(request, 'datos.html', {'form': DatosForm, 'form1': ProductForm})
+    else:
+        try:
+            form = DatosForm(request.POST)
+            new_datos = form.save(commit=False)
+            new_datos.user = request.user
+            new_datos.save() 
+            return redirect('datos')
+        except ValueError:
+            return render(request, 'datos.html', {'form': DatosForm, 'error':'Por favor ingresos los datos validos'})
+    
+@login_required
+def producto(request):
+    print('siii')
+    if request.method == 'GET':
+        return render(request, 'datos.html', {'form': DatosForm, 'form1': ProductForm})
+    else:
+        print('post')
+        try:
+            form1 = ProductForm(request.POST, request.FILES)
+            new_producto = form1.save(commit=False)
+            new_producto.user = request.user
+            new_producto.save()
+            print('aca')
+            return redirect('datos')
+        except ValueError:
+            return render(request, 'datos.html', {'form1': ProductForm, 'error':'Por favor ingresos los datos validos'})
+ 
 
 @login_required        
 def complete_task (request, task_id):
@@ -107,12 +161,14 @@ def complete_task (request, task_id):
         task.save()
         return redirect('tasks')
 
+
 @login_required
 def delete (request, task_id):
     task = get_object_or_404(Task, pk=task_id, user=request.user)  
     if request.method == 'POST':
         task.delete()
         return redirect('tasks')
+    
     
 @login_required
 def taskcomplete (request):
@@ -124,6 +180,7 @@ def taskcomplete (request):
 def tienda(request):
     productos = Producto.objects.all()
     return render(request, "tienda.html", {'productos': productos})
+
 
 def agregar_producto(request, producto_id):
     carrito = Carrito(request)
@@ -140,16 +197,19 @@ def eliminar_producto(request, producto_id):
    
     return redirect("cart")
     
+    
 def restar_producto(request, producto_id):
     carrito = Carrito(request)
     producto = Producto.objects.get(id=producto_id)
     carrito.restar(producto)
     return redirect("Tienda")
 
+
 def limpiar_carrito(request):
     carrito = Carrito(request)
     carrito.limpiar()
     return redirect("Tienda")
+
 
 def limpiar_carrito_item(request,producto_id):
     
@@ -158,10 +218,12 @@ def limpiar_carrito_item(request,producto_id):
     carrito.limpiaritem(producto)
     return redirect("cart")
 
+
 def galeriaprueba(request):
     productos = Producto.objects.all()
    
     return render(request, "gallery.html", {'productos': productos})
+
 
 def detalleproducto(request,producto_id):
     producto = Producto.objects.get(id=producto_id)
@@ -169,10 +231,6 @@ def detalleproducto(request,producto_id):
     return render(request, "productdetails.html", {'productos': producto})
     
     
-
-
-
-
 def cart(request):
     precioanterior = 0
     cantidad = 0
@@ -203,9 +261,9 @@ def cart(request):
         
         sdk = mercadopago.SDK("APP_USR-5213772683732349-061323-dc5bd7f2a56c2080735653bb6d1901e7-97277305")
         preference_data["back_urls"] = {
-        "success": "https://www.tu-sitio/success",
-        "failure": "https://www.tu-sitio/failure",
-        "pending": "https://www.tu-sitio/pendings"
+        "success": "http://127.0.0.1:8000/home/",
+        "failure": "http://127.0.0.1:8000/cart/",
+        "pending": "http://127.0.0.1:8000/home/"
     }
         preference_data["auto_return"] = "approved"
         
@@ -254,3 +312,30 @@ def cotizar(request):
         return JsonResponse({'message': 'Cotización exitosa'})  # Puedes retornar una respuesta JSON si lo deseas
 
     return render(request, 'cart.html')  # Reemplaza 'tu_template.html' con la plantilla adecuada
+
+
+def sendmail(request):
+    if request.method == 'POST':
+        mail = request.POST.get('mail')
+    asunto= 'Confirmación de Compra'
+    cuerpo = 'Gracias por tu compra. Hemos enviado un correo de confirmación a tu dirección: ' + 'correo_destinatario'
+    mensaje = f'{cuerpo}Si no recibes el correo, por favor comunícate con nosotros por WhatsApp al número +123456789.'
+    cel = '11-2394-1223'
+    
+    subject = asunto
+    message = mensaje
+    from_email = 'notificaciondepaginaweb@gmail.com'
+    recipient_list = ['notificaciondepaginaweb@gmail.com','maximobatallan@gmail.com']
+
+    correo= 'correo@correo.com'
+    
+    send_mail(subject, message, from_email, recipient_list)
+    
+    return render(request, "sendmail.html", {'asunto': asunto,'mensaje': mensaje, 'correo': correo, 'cel': cel})
+
+def datosbanco(request):
+    datos_relevantes = DatosPersonales.objects.filter(id=6).values('cbu', 'titular')
+    cbu = datos_relevantes[0]['cbu']
+    titular = datos_relevantes[0]['titular']
+    
+    return render(request, "datosbanco.html", {'cbu': cbu, 'titular': titular})
